3547c1e7a27c813da1917fd953cda860905887c6
Anders Brander <anders@brander.dk>
2015-09-23 00:30:11 +0200

    Added agento.GatherDuration to plugins.Plugin.

--- plugins/Plugin.go
+++ plugins/Plugin.go
@@ -2,8 +2,11 @@ package plugins
 
 import (
 	"log"
+	"time"
 
 	"github.com/influxdb/influxdb/client"
+
+	"github.com/abrander/agento"
 )
 
 type Plugin interface {
@@ -29,11 +32,28 @@ func Register(shortName string, constructor PluginConstructor) {
 
 func GatherAll() Results {
 	var results = Results{}
+	start := time.Now()
 
 	for name, p := range plugins {
 		p.Gather()
 		results[name] = p
 	}
 
+	results["g"] = time.Now().Sub(start)
+
 	return results
+}
+
+type GatherDuration time.Duration
+
+func (g *GatherDuration) Gather() error {
+	return nil
+}
+
+func (g *GatherDuration) GetPoints() []client.Point {
+	points := make([]client.Point, 1)
+
+	points[0] = agento.SimplePoint("agento.GatherDuration", agento.Round(time.Duration(*g).Seconds()*1000.0, 1))
+
+	return points
 }
