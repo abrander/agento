6b00e9209bae44f69a195fb6333ae867f88aa770
Anders Brander <anders@brander.dk>
2015-09-22 21:34:10 +0200

    Ported CpuSpeed to plugins.Plugin.

--- plugins/cpuspeed/CpuSpeed.go
+++ plugins/cpuspeed/CpuSpeed.go
@@ -1,4 +1,4 @@
-package agento
+package cpuspeed
 
 import (
 	"encoding/json"
@@ -8,39 +8,49 @@ import (
 	"strings"
 
 	"github.com/influxdb/influxdb/client"
+
+	"github.com/abrander/agento"
+	"github.com/abrander/agento/plugins"
 )
+
+func init() {
+	plugins.Register("c", NewCpuSpeed)
+}
+
+func NewCpuSpeed() plugins.Plugin {
+	return new(CpuSpeed)
+}
 
 type CpuSpeed struct {
 	Frequency []int
 }
 
-func GetCpuSpeed() *CpuSpeed {
-	speed := CpuSpeed{}
-
+func (c *CpuSpeed) Gather() error {
 	files, err := filepath.Glob("/sys/devices/system/cpu/cpu[0-9]*/cpufreq/cpuinfo_max_freq")
 
 	if err != nil {
-		return &speed
+		return err
 	}
 
-	speed.Frequency = make([]int, len(files))
+	c.Frequency = make([]int, len(files))
 	i := 0
 	for _, file := range files {
 		contents, err := ioutil.ReadFile(file)
 		if err == nil {
-			speed.Frequency[i], _ = strconv.Atoi(strings.TrimSpace(string(contents)))
+			c.Frequency[i], _ = strconv.Atoi(strings.TrimSpace(string(contents)))
 			i += 1
 		}
 	}
 
-	return &speed
+	return nil
 }
+
 func (c CpuSpeed) MarshalJSON() ([]byte, error) {
 	return json.Marshal(c.Frequency)
 }
 
 func (c *CpuSpeed) UnmarshalJSON(b []byte) error {
-	c.Frequency = make([]int, 100)
+	c.Frequency = make([]int, 128)
 	return json.Unmarshal(b, &c.Frequency)
 }
 
@@ -48,7 +58,7 @@ func (c *CpuSpeed) GetPoints() []client.Point {
 	points := make([]client.Point, len(c.Frequency))
 
 	for i, frequency := range c.Frequency {
-		points[i] = PointWithTag("cpu.Frequency", frequency, "core", strconv.Itoa(i))
+		points[i] = agento.PointWithTag("cpu.Frequency", frequency, "core", strconv.Itoa(i))
 	}
 
 	return points
