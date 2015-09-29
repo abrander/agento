69dcec941075d525eb7b170e1148224b34c2a8d2
Anders brander <anders@brander.dk>
2015-09-29 19:13:03 +0200

    Restrict TLS ciphersuites.

--- main.go
+++ main.go
@@ -165,6 +165,16 @@ func main() {
 			// Listen for https connections if needed
 			tlsConfig := &tls.Config{
 				MinVersion: tls.VersionTLS12,
+				CipherSuites: []uint16{
+					tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				},
 			}
 			addr := config.Server.Https.Bind + ":" + strconv.Itoa(int(config.Server.Https.Port))
 			LogInfo("Listening for https at " + addr)
