670150c7933ddcdc66fae818205d5f2aefffb33e
Anders Brander <anders@brander.dk>
2016-01-13 23:56:26 +0100

    Better random for small intervals.

--- client/client.go
+++ client/client.go
@@ -20,7 +20,7 @@ func GatherAndReport(clientConfig configuration.ClientConfiguration) {
 	logger.Yellow("client", "agento client started, reporting to %s", clientConfig.ServerUrl)
 
 	// Randomize our start time to avoid a big cluster reporting at the exact same time
-	time.Sleep(time.Second * time.Duration(rand.Intn(clientConfig.Interval)))
+	time.Sleep(time.Duration(rand.Intn(int(time.Second) * clientConfig.Interval)))
 
 	c := time.Tick(time.Second * time.Duration(clientConfig.Interval))
 	for _ = range c {
