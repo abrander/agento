6ee93e3c3a9249d674ba0a285a248cf58c6e10dd
Anders Brander <anders@brander.dk>
2016-02-01 21:11:05 +0100

    Added GetHostByName to monitor.Admin interface.

--- monitor/Monitor.go
+++ monitor/Monitor.go
@@ -29,6 +29,7 @@ type (
 		GetAllHosts(subject userdb.Subject, accountId string) ([]Host, error)
 		AddHost(subject userdb.Subject, host *Host) error
 		GetHost(subject userdb.Subject, id string) (*Host, error)
+		GetHostByName(subject userdb.Subject, name string) (*Host, error)
 		DeleteHost(subject userdb.Subject, id string) error
 	}
 
