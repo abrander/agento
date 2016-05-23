2575bd0bfbb5073c8b55d5eb01c962f7d455291f
Anders Brander <anders@brander.dk>
2016-05-23 22:26:43 +0200

    Added GetAgent() and GetTransport().

--- plugins/Agent.go
+++ plugins/Agent.go
@@ -1,6 +1,8 @@
 package plugins
 
 import (
+	"errors"
+
 	"github.com/influxdata/influxdb/client/v2"
 )
 
@@ -12,3 +14,24 @@ type (
 		GetPoints() []*client.Point
 	}
 )
+
+// GetAgent will return an agent of type id or nil plus an error if the
+// agent was not found.
+func GetAgent(id string) (Agent, error) {
+	// Try to find a constructor.
+	c, found := pluginConstructors[id]
+	if !found {
+		return nil, errors.New("Agent " + id + " not found")
+	}
+
+	// Instantiate.
+	plugin := c()
+
+	// Check if the plugin is in fact an agent.
+	agent, ok := plugin.(Agent)
+	if !ok {
+		return nil, errors.New("Agent " + id + " not found")
+	}
+
+	return agent, nil
+}
--- plugins/Transport.go
+++ plugins/Transport.go
@@ -1,6 +1,7 @@
 package plugins
 
 import (
+	"errors"
 	"io"
 	"net"
 	"syscall"
@@ -18,3 +19,24 @@ type (
 		Statfs(path string, buf *syscall.Statfs_t) error
 	}
 )
+
+// GetTransport will return a transport of type id or nil plus an error if the
+// transport was not found.
+func GetTransport(id string) (Transport, error) {
+	// Try to find a constructor.
+	c, found := pluginConstructors[id]
+	if !found {
+		return nil, errors.New("Transport " + id + " not found")
+	}
+
+	// Instantiate.
+	plugin := c()
+
+	// Check if the plugin is in fact a transport.
+	transport, ok := plugin.(Transport)
+	if !ok {
+		return nil, errors.New("Transport " + id + " not found")
+	}
+
+	return transport, nil
+}
