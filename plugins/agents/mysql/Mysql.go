abrander/agento

commit 729dc51a5864bcdc0207f1cbcd2b36e17f440c6b

Only report Galera metrics when we're participating in a Galera cluster.

plugins/agents/mysql/Mysql.go
@@ -294,34 +294,38 @@ func (m *Mysql) GetPoints() []*timeseries.Point {
 	points[28] = plugins.SimplePoint("mysql.OpenTables", m.OpenTables)
 	points[29] = plugins.SimplePoint("mysql.OpenedTables", m.OpenedTables)
 
-	points[30] = plugins.SimplePoint("mysql.WsrepOutOfOrderApply", m.WsrepOutOfOrderApply)
-	points[31] = plugins.SimplePoint("mysql.WsrepApplyWindow", m.WsrepApplyWindow)
-	points[32] = plugins.SimplePoint("mysql.WsrepCertDistance", m.WsrepCertDistance)
-	points[33] = plugins.SimplePoint("mysql.WsrepCertInterval", m.WsrepCertInterval)
-	points[34] = plugins.SimplePoint("mysql.WsrepConfigurationChanges", m.WsrepConfigurationChanges)
-	points[35] = plugins.SimplePoint("mysql.WsrepClusterSize", m.WsrepClusterSize)
-	points[36] = plugins.SimplePoint("mysql.WsrepNodeStatus", m.WsrepNodeStatus)
-	points[37] = plugins.SimplePoint("mysql.WsrepConnected", m.WsrepConnected)
-	points[38] = plugins.SimplePoint("mysql.WsrepEvicted", m.WsrepEvicted)
-	points[39] = plugins.SimplePoint("mysql.WsrepFlowPaused", m.WsrepFlowPaused)
-	points[40] = plugins.SimplePoint("mysql.WsrepFlowControlReceived", m.WsrepFlowControlReceived)
-	points[41] = plugins.SimplePoint("mysql.WsrepFlowControlSent", m.WsrepFlowControlSent)
-	points[42] = plugins.SimplePoint("mysql.WsrepTransactionsAborted", m.WsrepTransactionsAborted)
-	points[43] = plugins.SimplePoint("mysql.WsrepCertFailures", m.WsrepCertFailures)
-	points[44] = plugins.SimplePoint("mysql.WsrepCommits", m.WsrepCommits)
-	points[45] = plugins.SimplePoint("mysql.WsrepRxQueueLength", m.WsrepRxQueueLength)
-	points[46] = plugins.SimplePoint("mysql.WsrepTransactionReplays", m.WsrepTransactionReplays)
-	points[47] = plugins.SimplePoint("mysql.WsrepTxQueueLength", m.WsrepTxQueueLength)
-	points[48] = plugins.SimplePoint("mysql.WsrepState", m.WsrepState)
-	points[49] = plugins.SimplePoint("mysql.WsrepReady", m.WsrepReady)
-	points[50] = plugins.SimplePoint("mysql.WsrepWritesetsReceived", m.WsrepWritesetsReceived)
-	points[51] = plugins.SimplePoint("mysql.WsrepWritesetsSent", m.WsrepWritesetsSent)
-	points[52] = plugins.SimplePoint("mysql.WsrepThreadCount", m.WsrepThreadCount)
-	points[53] = plugins.SimplePoint("mysql.WsrepReplicationLatencyMinimum", m.WsrepReplicationLatencyMinimum)
-	points[54] = plugins.SimplePoint("mysql.WsrepReplicationLatencyAverage", m.WsrepReplicationLatencyAverage)
-	points[55] = plugins.SimplePoint("mysql.WsrepReplicationLatencyMaximum", m.WsrepReplicationLatencyMaximum)
-	points[56] = plugins.SimplePoint("mysql.WsrepReplicationLatencyStandardDeviation", m.WsrepReplicationLatencyStandardDeviation)
-	points[57] = plugins.SimplePoint("mysql.WsrepReplicationLatencySampleSize", m.WsrepReplicationLatencySampleSize)
+	// Only return these points if we're actually in a Galera-cluster. If the
+	// cluster size is zero we assume that no cluster is active.
+	if m.WsrepClusterSize > 0 {
+		points[30] = plugins.SimplePoint("mysql.WsrepOutOfOrderApply", m.WsrepOutOfOrderApply)
+		points[31] = plugins.SimplePoint("mysql.WsrepApplyWindow", m.WsrepApplyWindow)
+		points[32] = plugins.SimplePoint("mysql.WsrepCertDistance", m.WsrepCertDistance)
+		points[33] = plugins.SimplePoint("mysql.WsrepCertInterval", m.WsrepCertInterval)
+		points[34] = plugins.SimplePoint("mysql.WsrepConfigurationChanges", m.WsrepConfigurationChanges)
+		points[35] = plugins.SimplePoint("mysql.WsrepClusterSize", m.WsrepClusterSize)
+		points[36] = plugins.SimplePoint("mysql.WsrepNodeStatus", m.WsrepNodeStatus)
+		points[37] = plugins.SimplePoint("mysql.WsrepConnected", m.WsrepConnected)
+		points[38] = plugins.SimplePoint("mysql.WsrepEvicted", m.WsrepEvicted)
+		points[39] = plugins.SimplePoint("mysql.WsrepFlowPaused", m.WsrepFlowPaused)
+		points[40] = plugins.SimplePoint("mysql.WsrepFlowControlReceived", m.WsrepFlowControlReceived)
+		points[41] = plugins.SimplePoint("mysql.WsrepFlowControlSent", m.WsrepFlowControlSent)
+		points[42] = plugins.SimplePoint("mysql.WsrepTransactionsAborted", m.WsrepTransactionsAborted)
+		points[43] = plugins.SimplePoint("mysql.WsrepCertFailures", m.WsrepCertFailures)
+		points[44] = plugins.SimplePoint("mysql.WsrepCommits", m.WsrepCommits)
+		points[45] = plugins.SimplePoint("mysql.WsrepRxQueueLength", m.WsrepRxQueueLength)
+		points[46] = plugins.SimplePoint("mysql.WsrepTransactionReplays", m.WsrepTransactionReplays)
+		points[47] = plugins.SimplePoint("mysql.WsrepTxQueueLength", m.WsrepTxQueueLength)
+		points[48] = plugins.SimplePoint("mysql.WsrepState", m.WsrepState)
+		points[49] = plugins.SimplePoint("mysql.WsrepReady", m.WsrepReady)
+		points[50] = plugins.SimplePoint("mysql.WsrepWritesetsReceived", m.WsrepWritesetsReceived)
+		points[51] = plugins.SimplePoint("mysql.WsrepWritesetsSent", m.WsrepWritesetsSent)
+		points[52] = plugins.SimplePoint("mysql.WsrepThreadCount", m.WsrepThreadCount)
+		points[53] = plugins.SimplePoint("mysql.WsrepReplicationLatencyMinimum", m.WsrepReplicationLatencyMinimum)
+		points[54] = plugins.SimplePoint("mysql.WsrepReplicationLatencyAverage", m.WsrepReplicationLatencyAverage)
+		points[55] = plugins.SimplePoint("mysql.WsrepReplicationLatencyMaximum", m.WsrepReplicationLatencyMaximum)
+		points[56] = plugins.SimplePoint("mysql.WsrepReplicationLatencyStandardDeviation", m.WsrepReplicationLatencyStandardDeviation)
+		points[57] = plugins.SimplePoint("mysql.WsrepReplicationLatencySampleSize", m.WsrepReplicationLatencySampleSize)
+	}
 
 	return points
 }

commit 89bd8edde14d6965f95935398377006d3a6ddfa9

Fixed stupid bug in 729dc51a5 caused uninitialized points when gathering from a non-Galera-host.

plugins/agents/mysql/Mysql.go
@@ -258,7 +258,7 @@ func (m *Mysql) Gather(transport plugins.Transport) error {
 }
 
 func (m *Mysql) GetPoints() []*timeseries.Point {
-	points := make([]*timeseries.Point, 58)
+	points := make([]*timeseries.Point, 30, 58)
 
 	points[0] = plugins.SimplePoint("mysql.Connections", m.Connections)
 	points[1] = plugins.SimplePoint("mysql.AccessDeniedErrors", m.AccessDeniedErrors)
@@ -297,34 +297,38 @@ func (m *Mysql) GetPoints() []*timeseries.Point {
 	// Only return these points if we're actually in a Galera-cluster. If the
 	// cluster size is zero we assume that no cluster is active.
 	if m.WsrepClusterSize > 0 {
-		points[30] = plugins.SimplePoint("mysql.WsrepOutOfOrderApply", m.WsrepOutOfOrderApply)
-		points[31] = plugins.SimplePoint("mysql.WsrepApplyWindow", m.WsrepApplyWindow)
-		points[32] = plugins.SimplePoint("mysql.WsrepCertDistance", m.WsrepCertDistance)
-		points[33] = plugins.SimplePoint("mysql.WsrepCertInterval", m.WsrepCertInterval)
-		points[34] = plugins.SimplePoint("mysql.WsrepConfigurationChanges", m.WsrepConfigurationChanges)
-		points[35] = plugins.SimplePoint("mysql.WsrepClusterSize", m.WsrepClusterSize)
-		points[36] = plugins.SimplePoint("mysql.WsrepNodeStatus", m.WsrepNodeStatus)
-		points[37] = plugins.SimplePoint("mysql.WsrepConnected", m.WsrepConnected)
-		points[38] = plugins.SimplePoint("mysql.WsrepEvicted", m.WsrepEvicted)
-		points[39] = plugins.SimplePoint("mysql.WsrepFlowPaused", m.WsrepFlowPaused)
-		points[40] = plugins.SimplePoint("mysql.WsrepFlowControlReceived", m.WsrepFlowControlReceived)
-		points[41] = plugins.SimplePoint("mysql.WsrepFlowControlSent", m.WsrepFlowControlSent)
-		points[42] = plugins.SimplePoint("mysql.WsrepTransactionsAborted", m.WsrepTransactionsAborted)
-		points[43] = plugins.SimplePoint("mysql.WsrepCertFailures", m.WsrepCertFailures)
-		points[44] = plugins.SimplePoint("mysql.WsrepCommits", m.WsrepCommits)
-		points[45] = plugins.SimplePoint("mysql.WsrepRxQueueLength", m.WsrepRxQueueLength)
-		points[46] = plugins.SimplePoint("mysql.WsrepTransactionReplays", m.WsrepTransactionReplays)
-		points[47] = plugins.SimplePoint("mysql.WsrepTxQueueLength", m.WsrepTxQueueLength)
-		points[48] = plugins.SimplePoint("mysql.WsrepState", m.WsrepState)
-		points[49] = plugins.SimplePoint("mysql.WsrepReady", m.WsrepReady)
-		points[50] = plugins.SimplePoint("mysql.WsrepWritesetsReceived", m.WsrepWritesetsReceived)
-		points[51] = plugins.SimplePoint("mysql.WsrepWritesetsSent", m.WsrepWritesetsSent)
-		points[52] = plugins.SimplePoint("mysql.WsrepThreadCount", m.WsrepThreadCount)
-		points[53] = plugins.SimplePoint("mysql.WsrepReplicationLatencyMinimum", m.WsrepReplicationLatencyMinimum)
-		points[54] = plugins.SimplePoint("mysql.WsrepReplicationLatencyAverage", m.WsrepReplicationLatencyAverage)
-		points[55] = plugins.SimplePoint("mysql.WsrepReplicationLatencyMaximum", m.WsrepReplicationLatencyMaximum)
-		points[56] = plugins.SimplePoint("mysql.WsrepReplicationLatencyStandardDeviation", m.WsrepReplicationLatencyStandardDeviation)
-		points[57] = plugins.SimplePoint("mysql.WsrepReplicationLatencySampleSize", m.WsrepReplicationLatencySampleSize)
+		wsrepPoints := make([]*timeseries.Point, 28)
+
+		wsrepPoints[0] = plugins.SimplePoint("mysql.WsrepOutOfOrderApply", m.WsrepOutOfOrderApply)
+		wsrepPoints[1] = plugins.SimplePoint("mysql.WsrepApplyWindow", m.WsrepApplyWindow)
+		wsrepPoints[2] = plugins.SimplePoint("mysql.WsrepCertDistance", m.WsrepCertDistance)
+		wsrepPoints[3] = plugins.SimplePoint("mysql.WsrepCertInterval", m.WsrepCertInterval)
+		wsrepPoints[4] = plugins.SimplePoint("mysql.WsrepConfigurationChanges", m.WsrepConfigurationChanges)
+		wsrepPoints[5] = plugins.SimplePoint("mysql.WsrepClusterSize", m.WsrepClusterSize)
+		wsrepPoints[6] = plugins.SimplePoint("mysql.WsrepNodeStatus", m.WsrepNodeStatus)
+		wsrepPoints[7] = plugins.SimplePoint("mysql.WsrepConnected", m.WsrepConnected)
+		wsrepPoints[8] = plugins.SimplePoint("mysql.WsrepEvicted", m.WsrepEvicted)
+		wsrepPoints[9] = plugins.SimplePoint("mysql.WsrepFlowPaused", m.WsrepFlowPaused)
+		wsrepPoints[10] = plugins.SimplePoint("mysql.WsrepFlowControlReceived", m.WsrepFlowControlReceived)
+		wsrepPoints[11] = plugins.SimplePoint("mysql.WsrepFlowControlSent", m.WsrepFlowControlSent)
+		wsrepPoints[12] = plugins.SimplePoint("mysql.WsrepTransactionsAborted", m.WsrepTransactionsAborted)
+		wsrepPoints[13] = plugins.SimplePoint("mysql.WsrepCertFailures", m.WsrepCertFailures)
+		wsrepPoints[14] = plugins.SimplePoint("mysql.WsrepCommits", m.WsrepCommits)
+		wsrepPoints[15] = plugins.SimplePoint("mysql.WsrepRxQueueLength", m.WsrepRxQueueLength)
+		wsrepPoints[16] = plugins.SimplePoint("mysql.WsrepTransactionReplays", m.WsrepTransactionReplays)
+		wsrepPoints[17] = plugins.SimplePoint("mysql.WsrepTxQueueLength", m.WsrepTxQueueLength)
+		wsrepPoints[18] = plugins.SimplePoint("mysql.WsrepState", m.WsrepState)
+		wsrepPoints[19] = plugins.SimplePoint("mysql.WsrepReady", m.WsrepReady)
+		wsrepPoints[20] = plugins.SimplePoint("mysql.WsrepWritesetsReceived", m.WsrepWritesetsReceived)
+		wsrepPoints[21] = plugins.SimplePoint("mysql.WsrepWritesetsSent", m.WsrepWritesetsSent)
+		wsrepPoints[22] = plugins.SimplePoint("mysql.WsrepThreadCount", m.WsrepThreadCount)
+		wsrepPoints[23] = plugins.SimplePoint("mysql.WsrepReplicationLatencyMinimum", m.WsrepReplicationLatencyMinimum)
+		wsrepPoints[24] = plugins.SimplePoint("mysql.WsrepReplicationLatencyAverage", m.WsrepReplicationLatencyAverage)
+		wsrepPoints[25] = plugins.SimplePoint("mysql.WsrepReplicationLatencyMaximum", m.WsrepReplicationLatencyMaximum)
+		wsrepPoints[26] = plugins.SimplePoint("mysql.WsrepReplicationLatencyStandardDeviation", m.WsrepReplicationLatencyStandardDeviation)
+		wsrepPoints[27] = plugins.SimplePoint("mysql.WsrepReplicationLatencySampleSize", m.WsrepReplicationLatencySampleSize)
+
+		points = append(points, wsrepPoints...)
 	}
 
 	return points