ad1c0924d17cba94e29dbfd5b25607c5a84e76d2
Anders Kvist <ak@cego.dk>
2018-01-18 09:14:54 +0100

    Better way of getting loadavg from uptime command

--- plugins/agents/muninpluginrunner/example.sh
+++ plugins/agents/muninpluginrunner/example.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-uptime | awk -F "[:,]" '{print "loadavg1.value" $8 "\nloadavg5.value" $9 "\nloadavg15.value" $10}'
+uptime | grep -ohe 'load average[s:][: ].*' | awk -F '[ ,]' '{print "loadavg1.value " $3 "\nloadavg5.value " $5 "\nloadavg15.value " $7}'
